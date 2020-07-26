9409e87e3b1b1d9db579c04f9deecaeda147c2bc
GeorgeTyupin <georgytyupin@ya.ru>
2020-07-26 22:17:28 +0300

    26.07.2020

--- raspisanie/script.js
+++
@@ -1,9 +0,0 @@
-main = document.querySelectorAll('.main .col')
-
-main.forEach(elem => {
-    if (elem.dataset.visible == 'false') {
-        
-        elem.classList.add('sleep')
-        console.log(elem.classList)
-    }
-});
