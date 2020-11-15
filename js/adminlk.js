4603a95afa9bff28c5e0578156f0763e159ff18d
GeorgeTyupin <geogytyupin@yande.ru>
2020-11-15 21:16:47 +0300

    15.11.2020

--- js/adminlk.js
+++ js/adminlk.js
@@ -1,3 +1,8 @@
+/*
+===================================================================================================================
+                                         Добавление элемнтов 
+===================================================================================================================
+*/
 function addRow () {
     contein = document.querySelector('.container');
  
@@ -40,20 +45,11 @@ function addRow () {
     row.appendChild(div);
  
 }
-/*
-function setEvent() {
-    let add = document.querySelector('.add').childNodes[3];
-    add.addEventListener('click' , () => { 
-        addRow();
-    })
- 
- 
-}
-*/
  
 days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
- 
+
 //cоздаем класс
+// отработка
 function newClass(){
     //создаем сам класс по нажатию на кнопку
     document.querySelector(".new_class").addEventListener('click', () => {
@@ -89,12 +85,91 @@ function newClass(){
     }); 
  
 }
- 
+
 //добавляем урок
 function newLesson() {
     $(this.parentNode).before('<input type="text" name="" id="" class="less">');
 }
 
+
+
+/* 
+===================================================================================================================
+                                         Рендер странички 
+===================================================================================================================
+*/
+//первый рендер странички превью классов
+function renderFirst(response){
+    console.log("рендер");
+    response = JSON.parse(response);
+    if(!response){
+        return
+    }
+    classes_name = [];
+    //рендер по спику имен классов
+    response.forEach(elem => {
+        str = elem.slice(-5);
+        elem = elem.substr(0, elem.length - str.length);
+        classes_name.push(elem);
+        createMinClass(elem);
+    });
+    createMenuElem(classes_name);
+    //Вызов функции проверяющей открыто ли наше расписание
+    $(".triangle-right").bind('click', chekingOpenClass);
+}
+
+//Создание превью класса
+function createMinClass(name){
+    $('.prev').append('<div class="roll_class"></div>');
+    temp = $('.roll_class');
+    temp = temp[temp.length - 1];
+    $(temp).append('<div class="row"></div>');
+    $(temp).append('<div class="class_content row"></div>');
+    sub_temp = $(temp).children()[0];
+    $(sub_temp).append('<div class="triangle-right button"></div>');
+    $(sub_temp).append(`<input type="text" class="class_number" value=${name}>`);
+    $(sub_temp).append('<button  class="submit" onclick="saveClass()">Сохранить</button>');
+}
+
+//рендеринг расписания
+function renderSecond(response){
+    response = JSON.parse(response);
+    if(!response){
+        return
+    }
+    class_rasp = $(`input[value=${response['name']}]`).parent().parent();  
+    days.forEach(elem => {
+        console.log(elem)
+        $($(class_rasp).children()[1]).append(`<div class="col-2 days">${elem}</div>`);
+    });
+    //$($(class_rasp).children()[1]).append('<div class="col-2 days">Понедельник</div>');
+    }
+
+//создание блока с названием класса в боковое меню
+function createMenuElem(classes){
+    classes.forEach(elem => {
+        console.log(elem);
+        $('.menu').append(`<div class="menu_class-1">${elem}</div>`);
+    });
+}
+ 
+//проверка открыто ли закрыто расписание у класса
+function chekingOpenClass(event) {
+    if (event.target.classList.contains('open') == false && event.target.classList.contains('close') == false) {
+        secondLoadData(event.target);
+        event.target.classList.add('open');
+        console.log(event.target.classList);
+    }
+    else {
+       $( $(event.target.parentNode.parentNode).children()[1]).slideToggle();
+    }
+}
+
+
+
+/*===================================================================================================================
+                                         Работа с сервером 
+===================================================================================================================*/
 //собираем данные и формируем массив для отправки на сервер
 function saveClass() {
     contain = event.target.parentNode.parentNode;
@@ -134,84 +209,9 @@ function saveClass() {
     }
     sendData(data_class);
 }
- 
-//Создание превью класса
-function createMinClass(name){
-    $('.prev').append('<div class="roll_class"></div>');
-    temp = $('.roll_class');
-    temp = temp[temp.length - 1];
-    $(temp).append('<div class="row"></div>');
-    $(temp).append('<div class="class_content row"></div>');
-    sub_temp = $(temp).children()[0];
-    $(sub_temp).append('<div class="triangle-right button"></div>');
-    $(sub_temp).append(`<input type="text" class="class_number" value=${name}>`);
-    $(sub_temp).append('<button  class="submit" onclick="saveClass()">Сохранить</button>');
-}
-
-//проверка открыто ли закрыто расписание у класса
-function chekingOpenClass(event) {
-    if (event.target.classList.contains('open') == false || event.target.classList.contains('close') == false) {
-        secondLoadData(event.target);
-        event.target.classList.add('open');
-        console.log(event.target.classList);
-    }
-    else if (event.target.classList.contains('open') == false && event.target.classList.contains('close') == true) {
-        //здесь должен быть вызов функции, которя будет разворачивать расписание
-    }
-    else if (event.target.classList.contains('open') == true && event.target.classList.contains('close') == false) {
-        //здесь должен быть вызов функции, которя будет сворачивать расписание
-    }
-}
-
-//создание блока с названием класса в боковое меню
-function createMenuElem(classes){
-    classes.forEach(elem => {
-        console.log(elem);
-        $('.menu').append(`<div class="menu_class-1">${elem}</div>`);
-    });
-}
- 
-//Отладка для ответа отправки данных
-function success(response){
-    console.log(response);
-}
- //сохраниение классов
+//сохраниение классов
 function sendData(data) {
     $.post("inputData.php", {data : data}, success = success);
-}
-//первый рендер странички превью классов
-function renderFirst(response){
-    console.log("рендер");
-    response = JSON.parse(response);
-    if(!response){
-        return
-    }
-    classes_name = [];
-    //рендер по спику имен классов
-    response.forEach(elem => {
-        str = elem.slice(-5);
-        elem = elem.substr(0, elem.length - str.length);
-        classes_name.push(elem);
-        createMinClass(elem);
-    });
-    createMenuElem(classes_name);
-    //Вызов функции проверяющей открыто ли наше расписание
-    $(".triangle-right").bind('click', chekingOpenClass);
-}
-
-//рендеринг расписания
-function renderSecond(response){
-    response = JSON.parse(response);
-    if(!response){
-        return
-    }
-    class_rasp = $(`input[value=${response['name']}]`).parent().parent();  
-    days.forEach(elem => {
-        console.log(elem)
-        $($(class_rasp).children()[1]).append(`<div class="col-2 days">${elem}</div>`);
-    });
-    //$($(class_rasp).children()[1]).append('<div class="col-2 days">Понедельник</div>');
-    
 }
 
 //получение с сервера структуры классов
@@ -224,7 +224,8 @@ function secondLoadData(temp) {
 function firstLoadData(){
     $.post("firstLoadData.php" , success = renderFirst);
 }
- 
+
+
 function main() {
     //setEvent();
     firstLoadData();
